a40e1c94075bf593787243a2889909a54cd9f643
WIN-SEUNGCHAN-J\seungchan.jeong <seungchan.jeong@unity3d.com>
2022-10-26 20:11:55 +0900

    foliagetype 검사 추가
    (cherry picked from commit 2b28cfc057501d0e840ae43afc56a63ee10bc8fb)

--- Assets/Scripts/ShaderFunction/IndirectPos.hlsl
+++ Assets/Scripts/ShaderFunction/IndirectPos.hlsl
@@ -32,6 +32,7 @@ struct InstanceData {
 	int foliageType;
 };
 
+int _FoliageType;
 StructuredBuffer<InstanceData> _PerInstanceData;
 // Stores the matrices (and possibly other data) sent from the C# side via material.SetBuffer, in Start/OnEnable.
 // See : https://gist.github.com/Cyanilux/e7afdc5c65094bfd0827467f8e4c3c54
@@ -48,30 +49,41 @@ StructuredBuffer<InstanceData> _PerInstanceData;
 	void vertInstancingMatrices(inout float4x4 objectToWorld, out float4x4 worldToObject) {
 		InstanceData data = _PerInstanceData[unity_InstanceID];
 
-		objectToWorld = mul(objectToWorld, data.m);
+		if(_FoliageType == data.foliageType)
+		{
+			objectToWorld = mul(objectToWorld, data.m);
 
-		// Transform matrix (override current)
-		// I prefer keeping positions relative to the bounds passed into DrawMeshInstancedIndirect so use the above instead
-		//objectToWorld._11_21_31_41 = float4(data.m._11_21_31, 0.0f);
-		//objectToWorld._12_22_32_42 = float4(data.m._12_22_32, 0.0f);
-		//objectToWorld._13_23_33_43 = float4(data.m._13_23_33, 0.0f);
-		//objectToWorld._14_24_34_44 = float4(data.m._14_24_34, 1.0f);
+			// Transform matrix (override current)
+			// I prefer keeping positions relative to the bounds passed into DrawMeshInstancedIndirect so use the above instead
+			//objectToWorld._11_21_31_41 = float4(data.m._11_21_31, 0.0f);
+			//objectToWorld._12_22_32_42 = float4(data.m._12_22_32, 0.0f);
+			//objectToWorld._13_23_33_43 = float4(data.m._13_23_33, 0.0f);
+			//objectToWorld._14_24_34_44 = float4(data.m._14_24_34, 1.0f);
 
-		// Inverse transform matrix
-		float3x3 w2oRotation;
-		w2oRotation[0] = objectToWorld[1].yzx * objectToWorld[2].zxy - objectToWorld[1].zxy * objectToWorld[2].yzx;
-		w2oRotation[1] = objectToWorld[0].zxy * objectToWorld[2].yzx - objectToWorld[0].yzx * objectToWorld[2].zxy;
-		w2oRotation[2] = objectToWorld[0].yzx * objectToWorld[1].zxy - objectToWorld[0].zxy * objectToWorld[1].yzx;
+			// Inverse transform matrix
+			float3x3 w2oRotation;
+			w2oRotation[0] = objectToWorld[1].yzx * objectToWorld[2].zxy - objectToWorld[1].zxy * objectToWorld[2].yzx;
+			w2oRotation[1] = objectToWorld[0].zxy * objectToWorld[2].yzx - objectToWorld[0].yzx * objectToWorld[2].zxy;
+			w2oRotation[2] = objectToWorld[0].yzx * objectToWorld[1].zxy - objectToWorld[0].zxy * objectToWorld[1].yzx;
 
-		float det = dot(objectToWorld[0].xyz, w2oRotation[0]);
-		w2oRotation = transpose(w2oRotation);
-		w2oRotation *= rcp(det);
-		float3 w2oPosition = mul(w2oRotation, -objectToWorld._14_24_34);
+			float det = dot(objectToWorld[0].xyz, w2oRotation[0]);
+			w2oRotation = transpose(w2oRotation);
+			w2oRotation *= rcp(det);
+			float3 w2oPosition = mul(w2oRotation, -objectToWorld._14_24_34);
 
-		worldToObject._11_21_31_41 = float4(w2oRotation._11_21_31, 0.0f);
-		worldToObject._12_22_32_42 = float4(w2oRotation._12_22_32, 0.0f);
-		worldToObject._13_23_33_43 = float4(w2oRotation._13_23_33, 0.0f);
-		worldToObject._14_24_34_44 = float4(w2oPosition, 1.0f);
+			worldToObject._11_21_31_41 = float4(w2oRotation._11_21_31, 0.0f);
+			worldToObject._12_22_32_42 = float4(w2oRotation._12_22_32, 0.0f);
+			worldToObject._13_23_33_43 = float4(w2oRotation._13_23_33, 0.0f);
+			worldToObject._14_24_34_44 = float4(w2oPosition, 1.0f);
+		}
+		else
+		{
+			//Temp. 이렇게 하지말고 shader 계산 자체를 아에 안하도록 해야함. (pipeline discard)
+			// float4x4 zeroMat = float4x4(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+			//
+			// objectToWorld = zeroMat;
+			// worldToObject = zeroMat;
+		}
 	}
 
 	void vertInstancingSetup() {
